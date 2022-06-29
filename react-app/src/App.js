631390862bc57036062c029009e381eae9c7558b
jiho <jhnam0422@naver.com>
2022-06-29 16:58:15 +0900

    toc event

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,12 +9,68 @@ import Image from './components/Image';
 import Button from './components/Button';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      mode:'read',
+      selected_content_id:2,
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  
   render(){
+    console.log('App render');
+    let _title, _desc=null;
+    if(this.state.mode === 'welcome'){
+        _title = this.state.welcome.title;
+        _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+        // _title = this.state.contents[0].title;
+        // _desc = this.state.contents[0].desc;
+        let i = 0;
+        while(i < this.state.contents.length){
+          let data = this.state.contents[i];
+          if(data.id === this.state.selected_content_id){
+            _title = data.title;
+            _desc = data.desc;
+            break;
+          }
+          i++;
+        }
+    }
     return (
       <div>
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject
+        title={this.state.subject.title}
+        sub={this.state.subject.sub}
+        onChangePage={function(){this.setState({mode:'welcome'})}.bind(this)}>
+        </Subject>
+        {/* ---------------------------------------------------------------------------------------- */}
+        {/* <header> */}
+        {/* <h1><a href='/' onClick={function(e){ */}
+        {/*   e.preventDefault(); */}
+        {/*   this.setState({mode : 'welcome'}); */}
+        {/* }.bind(this)}>{this.state.subject.title}</a></h1> */}      {/* bind를 사용하지 않으면 this는 undefined된 객체이기 때문에 bind를 사용해야 한다. */}
+        {/* {this.state.subject.sub} */}
+        {/* </header> */}
+        {/* ---------------------------------------------------------------------------------------- */}
+        <TOC
+        data = {this.state.contents}
+        onChangePage={function(id){
+          this.setState({
+            mode:'read', 
+            selected_content_id:Number(id)},
+          )}
+          .bind(this)}
+        >  {/* 하위->상위 호출 :이벤트 사용, 상위->하위 호출:props사용//번거로운 과정을 줄이기 위해 형제 간 이동은 redux 사용 */}
+        </TOC>
+        <Content title={_title} desc={_desc}></Content>
         <Table></Table>
         <Image></Image>
         <Button></Button>
--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -2,12 +2,25 @@ import React,  { Component } from 'react';
 
 class TOC extends Component{
     render(){
+      console.log('TOC render');
+      let lists = [];
+      let data = this.props.data;
+
+      let i=0;
+      while(i < data.length){
+        lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}
+        data-id = {data[i].id}
+        onClick={function(e){
+          e.preventDefault();
+          /* e.target -->타겟이 된 대상을 가져올 수 있음 */
+          this.props.onChangePage(e.target.dataset.id);
+        }.bind(this)}>{data[i].title}</a></li>);
+        i = i + 1;
+      }
       return (
         <nav>
           <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
+            {lists}
           </ul>
         </nav>
       );
